8b7edab89187a44148b5e8515642f3dcc80f44c6
Leandro <lealmeids@gmail.com>
2020-12-27 17:47:24 -0400

    Adicionando log info de teste para o OnItemClick da lista de alunos

--- app/src/main/java/com/example/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/com/example/agenda/ui/activity/ListaAlunosActivity.java
@@ -2,7 +2,9 @@ package com.example.agenda.ui.activity;
 //Activity que mostra a lista de alunos(as) existentes
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -53,5 +55,11 @@ public class ListaAlunosActivity extends AppCompatActivity {
         ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
         listaDeAlunos.setAdapter(new ArrayAdapter<>(this,android.
                 R.layout.simple_list_item_1,dao.todos()));
+        listaDeAlunos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int posicao, long id) {
+                Log.i("posicao_aluno", "" + posicao);
+            }
+        });
     }
 }
